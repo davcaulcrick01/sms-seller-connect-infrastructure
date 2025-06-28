d69b344ef0cb57c6e21b43a0f36062a80d5ac74c
David  Caulcrick <davcaulcrick01@gmail.com>
2025-06-27 19:50:49 -0500

    pushing

--- modules/ec2/acm.tf
+++ modules/ec2/acm.tf
@@ -48,7 +48,7 @@ resource "aws_route53_record" "cert_validation" {
 resource "aws_acm_certificate_validation" "main" {
   certificate_arn         = aws_acm_certificate.main.arn
   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-  
+
   timeouts {
     create = "10m"
   }
