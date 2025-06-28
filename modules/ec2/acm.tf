606a5ce3d9c3c4bd7673c7953399472ba70acc88
David  Caulcrick <davcaulcrick01@gmail.com>
2025-06-27 19:49:18 -0500

    pushing

--- modules/ec2/acm.tf
+++ modules/ec2/acm.tf
@@ -44,12 +44,12 @@ resource "aws_route53_record" "cert_validation" {
   zone_id         = data.aws_route53_zone.main.zone_id
 }
 
-# Skip ACM certificate validation for faster deployment
-# Note: Certificate will be validated automatically by AWS
-# resource "aws_acm_certificate_validation" "main" {
-#   certificate_arn         = aws_acm_certificate.main.arn
-#   validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-#   timeouts {
-#     create = "10m"
-#   }
-# } 
+# ACM certificate validation - wait for certificate to be validated
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn         = aws_acm_certificate.main.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+  
+  timeouts {
+    create = "10m"
+  }
+} 
--- modules/ec2/alb.tf
+++ modules/ec2/alb.tf
@@ -102,7 +102,7 @@ resource "aws_lb_listener" "https" {
   port              = "443"
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-  certificate_arn   = aws_acm_certificate.main.arn
+  certificate_arn   = aws_acm_certificate_validation.main.certificate_arn
 
   default_action {
     type             = "forward"
